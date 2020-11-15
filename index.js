0d12806d05cda70d302d3730897a2007f1144c2d
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-11-15 22:17:12 +0100

    add express and webhook for player joined minecraft server

--- index.js
+++ index.js
@@ -3,6 +3,54 @@ const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const discordClient = new Discord.Client();
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 9786;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/**
+ *
+ * BODY
+ *
+ * {
+ *    "player_name": "p4dd9",
+ *    "game": {
+ *        "current_players": 2,
+ *        "max_players": 20
+ *    },
+ *    "timestamp":1605472293
+ * }
+ *
+ *
+ */
+
+app.post("/mcplayerjoined", (request, response) => {
+  const { timestamp, player_name, game } = request.body;
+  const { current_players, max_players } = game;
+
+  const date = new Date(timestamp * 1000);
+  const year = date.getFullYear();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const hours = date.getHours();
+  const minutes = ("0" + date.getMinutes()).substr(-2);
+  const seconds = ("0" + date.getSeconds()).substr(-2);
+  const formattedTime = `${day}/${month}/${year} um ${hours}:${minutes}:${seconds}`;
+  const message = `:pick: **${player_name}** joined the minecraft server! **${current_players}/${max_players}** players live. *[${formattedTime}]*`;
+  console.log(message);
+
+  sendMessageToMCChannel(message);
+
+  response.json({ requestBody: request.body });
+});
+
+app.listen(port, () => {
+  console.log(`Started on PORT ${port}`);
+});
+
 const TOKEN = Object.freeze({
   DISCORD: process.env.DISCORD_TOKEN,
   GIPHY: process.env.GIPHY_TOKEN,
@@ -13,11 +61,6 @@ const URLS = Object.freeze({
   GIPHY_RANDOM: "https://api.giphy.com/v1/gifs/random?api_key=" + TOKEN.GIPHY,
 });
 
-const getRandomGiphy = async () => {
-  const response = await fetch(URLS.GIPHY_RANDOM);
-  return (await response.json()).data.url;
-};
-
 const getRandomGiphyByTag = async (tag) => {
   const response = await fetch(`${URLS.GIPHY_RANDOM}&tag=${tag}`);
   return (await response.json()).data.url;
@@ -26,6 +69,11 @@ const getRandomGiphyByTag = async (tag) => {
 const fetchMCServerStatus = async () => {
   const response = await fetch(TOKEN.MC_STATUS_URL);
   return await response.json();
+};
+
+const sendMessageToMCChannel = function (message) {
+  const channel = discordClient.channels.cache.get("776203480880119818");
+  channel.send(message);
 };
 
 discordClient.login(TOKEN.DISCORD);

e32f95b93b1f2480a57e1b1f1f966c9ae4f07e42
Patrick Obermueller <pat.obermueller@gmail.com>
2021-03-28 13:26:16 +0200

    add eslint and airbnb shared config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,13 @@ module.exports = {
     filename: "bundle.js",
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+    ],
   },
   resolve: {
     extensions: [".js"],
